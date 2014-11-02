0e9c3855aadd62852bb298b86db350ff398e050c
deimon777 <blanco.damian@gmail.com>
2014-11-02 23:15:13 +0000

    Agregue los GET & SET, hay q ver cuales sirven

--- isfpp/src/hospital/Doctor.java
+++ isfpp/src/hospital/Doctor.java
@@ -1,6 +1,5 @@
 package hospital;
 
-//mostro de la laguna
 public class Doctor {
 	private int tipo ; // 0: F , 1: NF
 	private int tAtencion;
@@ -14,5 +13,29 @@ public class Doctor {
 	}
 	public boolean estaOcupado( int t ) {
 		return t < tLibre ;
+	}
+	/****************/
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int _tipo) {
+		this.tipo = _tipo;
+	}
+
+	public int getAtencion() {
+		return tAtencion;
+	}
+
+	public void setAtencion(int _tAtencion) {
+		this.tAtencion = _tAtencion;
+	}
+
+	public int gettLibre() {
+		return tLibre;
+	}
+
+	public void settLibre(int _tLibre) {
+		this.tLibre = _tLibre;
 	}
 }
